9a76e4a09c8d95406c306bdba2f9c5c48910fe37
2616479 <2616479@tcs.com>
2023-03-23 08:33:37 -0500

    Validate Join Today option

--- src/test/java/co/com/tcs/certification/utestupskilling/stepdefinitions/JoinTodayStepDefinitions.java
+++ src/test/java/co/com/tcs/certification/utestupskilling/stepdefinitions/JoinTodayStepDefinitions.java
@@ -1,14 +1,19 @@
 package co.com.tcs.certification.utestupskilling.stepdefinitions;
 
+import co.com.tcs.certification.utestupskilling.questions.ValidateOption;
 import co.com.tcs.certification.utestupskilling.userinterfaces.UtestPage;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import net.serenitybdd.screenplay.Consequence;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
+import org.hamcrest.Matchers;
 
 import static co.com.tcs.certification.utestupskilling.utils.Constants.ACTOR_NAME;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 
 public class JoinTodayStepDefinitions {
 
@@ -27,6 +32,7 @@ public class JoinTodayStepDefinitions {
 
 
     @Then("^The user should see (.*) option$")
-    public void theUserShouldSeeJoinTodayOption() {
+    public void theUserShouldSeeJoinTodayOption(String nameOption) {
+         OnStage.theActorInTheSpotlight().should(seeThat(ValidateOption.isPresent(), Matchers.equalTo(nameOption)));
     }
 }
